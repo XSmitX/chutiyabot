a2dc18988e6c1fddd6a5f2e6ebe76711f65685f4
Smit Panchal <91816167+XSmitX@users.noreply.github.com>
2024-08-04 00:06:29 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3-slim-buster
+
+RUN pip3 install --upgrade pip
+
+ENV USER botx
+ENV HOME /home/$USER
+ENV BOT $HOME/terabox
+
+RUN useradd -m $USER
+RUN chown $USER:$USER $BOT
+USER $USER
+WORKDIR $BOT
+
+COPY requirements.txt requirements.txt
+RUN pip3 install --user --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD python3 TeraboxBot.py
---
+++ bot.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton as ikb , InlineKeyboardMarkup as ikm
+
+
+bot = Client('BotServer2',api_id=1712043,
+    api_hash="965c994b615e2644670ea106fd31daaf",
+    bot_token='6634907060:AAG7-WcGVElolHfIcU8-rnLlFr90efKqb8o')
+
+@bot.on_message(filters.command('start'))
+async def start(bot, message):
+    buttons = [
+        [ikb("Get New ID On WhatsApp", url="https://wa.link/mahaveer_book_official")],
+        [ikb("Join Our Official Telegram Channel", url="https://t.me/+3MWySnnLWC0xYTFl")],
+        [ikb("Join Our Official Instagram Page", url="https://instagram.com/mahaveerbook_official?igshid=MzRlODBiNWFlZA==")],
+        [ikb("Create ID On Automatic Site", url="https://mahaveerexch.com")]]
+    await bot.send_photo(message.chat.id, 'https://postimg.cc/SXQDjWF5')
+    await bot.send_message(message.chat.id,'<b><i>💰WELCOME TO MAHAVEER EXCHANGE💰</b></i>')
+    await bot.send_message(message.chat.id,'<b>Please click the button below</b>',reply_markup =ikm(buttons))
+
+
+
+bot.run()
