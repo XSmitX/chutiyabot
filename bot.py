7d389e4b5431fef42c24933c9d2e50003344e0c4
Smit Panchal <91816167+XSmitX@users.noreply.github.com>
2024-08-06 20:18:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,11 @@ bot = Client('BotServer2',api_id=1712043,
 async def start(bot, message):
     buttons = [
         [ikb("Get New ID On WhatsApp", url="https://wa.link/mahaveer_book_official")],
+        [ikb("Create ID On Automatic Site", url="https://mahaveerexch.com")],
         [ikb("Join Our Official Telegram Channel", url="https://t.me/+3MWySnnLWC0xYTFl")],
-        [ikb("Join Our Official Instagram Page", url="https://instagram.com/mahaveerbook_official?igshid=MzRlODBiNWFlZA==")],
-        [ikb("Create ID On Automatic Site", url="https://mahaveerexch.com")]]
-    await bot.send_photo(message.chat.id, 'https://postimg.cc/SXQDjWF5')
-    await bot.send_message(message.chat.id,'<b><i>💰WELCOME TO MAHAVEER EXCHANGE💰</b></i>')
+        [ikb("Join Our Official Instagram Page", url="https://instagram.com/mahaveerbook_official?igshid=MzRlODBiNWFlZA==")]],
+    await bot.send_photo(message.chat.id, 'https://postimg.cc/SXQDjWF5',caption='<b><i>💰WELCOME TO MAHAVEER EXCHANGE💰</b></i>')
+    #await bot.send_message(message.chat.id,'<b><i>💰WELCOME TO MAHAVEER EXCHANGE💰</b></i>')
     await bot.send_message(message.chat.id,'<b>Please click the button below</b>',reply_markup =ikm(buttons))
 
 
